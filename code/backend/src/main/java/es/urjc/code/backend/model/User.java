89e780306ec443090681b249468842b41d6953be
Jorge <jorge.castellanobajo@gmail.com>
2026-03-30 18:16:45 +0200

    Update: User.java

--- code/backend/src/main/java/es/urjc/code/backend/model/User.java
+++ code/backend/src/main/java/es/urjc/code/backend/model/User.java
@@ -1,6 +1,8 @@
 package es.urjc.code.backend.model;
 
 import jakarta.persistence.*;
+import java.sql.Blob;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -10,6 +12,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String nickname;
     private String email;
@@ -18,8 +21,17 @@ public class User {
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles;
 
-    @ManyToOne
-    private Team team;
+    @Lob
+    private Blob imageFile;
+
+    @ManyToMany(mappedBy = "players")
+    private List<Team> teams = new ArrayList<>();
+
+    @ManyToMany
+    private List<Tournament> favoriteTournaments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL)
+    private List<Tournament> createdTournaments = new ArrayList<>();
 
     public User() {}
 
@@ -30,11 +42,35 @@ public class User {
         this.password = password;
         this.roles = roles;
     }
+    public List<Tournament> getCreatedTournaments() {
+        return createdTournaments;
+    }
+    public void setCreatedTournaments(List<Tournament> createdTournaments) {
+        this.createdTournaments = createdTournaments;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public List<Tournament> getFavoriteTournaments() {
+        return favoriteTournaments;
+    }
+    public void setFavoriteTournaments(List<Tournament> favoriteTournaments) {
+        this.favoriteTournaments = favoriteTournaments;
+    }
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
+    }
+    public Blob getImageFile() {
+        return imageFile;
+    }
+    public void setImageFile(Blob imageFile) {
+        this.imageFile = imageFile;
     }
     public String getName() {
         return name;
@@ -48,12 +84,6 @@ public class User {
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
     public String getPassword() {
         return password;
     }
@@ -66,10 +96,11 @@ public class User {
     public void setRoles(List<String> roles) {
         this.roles = roles;
     }
-    public Team getTeam() {
-        return team;
+    public List<Team> getTeams() {
+        return teams;
     }
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
     }
+
 }
