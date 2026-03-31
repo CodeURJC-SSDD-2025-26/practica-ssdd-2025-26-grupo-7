f08e2a00a603f65960450edc9ce18e79b46d86ea
Jorge <jorge.castellanobajo@gmail.com>
2026-03-31 11:03:53 +0200

    Update User.java

--- code/backend/src/main/java/es/urjc/code/backend/model/User.java
+++ code/backend/src/main/java/es/urjc/code/backend/model/User.java
@@ -1,18 +1,22 @@
 package es.urjc.code.backend.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import java.sql.Blob;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
     private String nickname;
     private String email;
@@ -41,6 +45,34 @@ public class User {
         this.email = email;
         this.password = password;
         this.roles = roles;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+    public String getUsername(){
+        return nickname;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
     public List<Tournament> getCreatedTournaments() {
         return createdTournaments;
